f1d1e6e7577a0ebde128e770c4eb5744be8dbfe6
Philippe <cecile_philippe@live.fr>
2016-09-29 19:52:35 +0200

    modification du 29 septembre 2016

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
-var species = {
-	cat    : "cat.jpg",
-	bear   : "bear.jpg",
-	fish   : "fish.jpg"
-};
 
-function main(){
-	/* 
+	
+	var species = {
+		cat    : "cat.jpg",
+		bear   : "bear.jpg",
+		fish   : "fish.jpg"
+		};
+
+$("button").click(function(){
+ 	var pandaType = $(this).data('animal');
+ 	var animal = species[pandaType];
+ 	$("img").attr('src', "img/" + animal);
+ 	console.log(animal)
+
+
+});
+
+
+
+
+/* 
 	tout votre code doit se trouver dans cette fonction,
 	vous pouvez biensur créer d'autres fonctions si nécessaire
 	*/
 
 	// 1. placez un listener sur le click des éléments <button>
-	// 2. dans le callback récupérez le data attribute animal
+	// 2. dans le callback récupérez le data attribute animal,
 	// 3. récupérerez dans l'objet species la valeur correspond à l'attribut récupéré
 	// 4. ajoutez élément img en assignant à son attibut src la valeur précédemment récupérée
 	// 5. inserez cet élément dans l'élément ayant l'id holder
@@ -19,9 +32,14 @@ function main(){
 	// Conseils : avancez étape par étape, console.log et debugger autant que nécessaires !
 	
 	
-	 
-}
+	
 
-$(document).ready(function(){
-	main();
+/*
+$("#panda").click(function(){
+	var pandaType = $(this).data('type');
+	console.log(pandaType)
+	ilove(pandaType);
 });
+*/
+
+
